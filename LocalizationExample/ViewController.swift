a67055e029695d7f19e353b721b0bc325cbfd78c
Jill Scott <jill.scott@productops.com>
2017-07-14 15:26:46 -0700

    Commented out alert section
    Alert using NSLocalizedString needs more work

--- LocalizationExample/ViewController.swift
+++ LocalizationExample/ViewController.swift
@@ -13,17 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let alertTitle = NSLocalizedString("Welcome", comment: "")
-        let alertMessage = NSLocalizedString("This is an alert!", comment: "")
-        let cancelButtonText = NSLocalizedString("Cancel", comment: "")
-        let signupButtonText = NSLocalizedString("Signup", comment: "")
-        
-        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.alert)
-        let cancelAction = UIAlertAction(title: cancelButtonText, style: UIAlertActionStyle.cancel, handler: nil)
-        let signupAction = UIAlertAction(title: signupButtonText, style: UIAlertActionStyle.default, handler: nil)
-        alert.addAction(cancelAction)
-        alert.addAction(signupAction)
-        present(alert, animated: true, completion: nil)
+//        let alertTitle = NSLocalizedString("Welcome", comment: "")
+//        let alertMessage = NSLocalizedString("This is an alert!", comment: "")
+//        let cancelButtonText = NSLocalizedString("Cancel", comment: "")
+//        let signupButtonText = NSLocalizedString("Signup", comment: "")
+//        
+//        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.alert)
+//        let cancelAction = UIAlertAction(title: cancelButtonText, style: UIAlertActionStyle.cancel, handler: nil)
+//        let signupAction = UIAlertAction(title: signupButtonText, style: UIAlertActionStyle.default, handler: nil)
+//        alert.addAction(cancelAction)
+//        alert.addAction(signupAction)
+//        present(alert, animated: true, completion: nil)
     
     }
 
